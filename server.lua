e26577884da5559c83bbf72b3d15f06f6b075f0e
31Ash <159731485+31Ash@users.noreply.github.com>
2026-06-06 16:26:29 +0100

    Add files via upload
    Update to script also returning impounded cars to garage

--- server.lua
+++ server.lua
@@ -2,10 +2,10 @@ CreateThread(function()
     Wait(5000) -- Give database resource time to initialize
 
     exports.oxmysql:update(
-        'UPDATE player_vehicles SET state = 0 WHERE state = 1',
+        'UPDATE player_vehicles SET state = 0 WHERE state = 1 AND isTowedOut = 0',
         {},
         function(affectedRows)
-            print(('[Ash-vehicle-reset] Reset %s vehicle states.'):format(affectedRows))
+            print(('[Ash-vehicle-reset] Reset %s vehicles to stored state.'):format(affectedRows))
         end
     )
 end)
